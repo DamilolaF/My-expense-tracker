8c0bd75b4a691b5714d1eb8030ef51d9e5e953aa
Damilola <dami_fagbo@yahoo.com>
2021-03-16 23:17:25 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-const balance = document.querySelector('#balance')
+//list all classes
+const balance = document.getElementById('balance');
+const moneyPlus = document.getElementById('money-plus');
+const moneyMinus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+//What the transactions will look like
+//const fake/transactions = [
+//  {id: 1, text: 'beauty', amount: -45},
+//  {id: 2, text: 'flower', amount: -50},
+//  {id: 3, text: 'Salary', amount: 4500},
+//  {id: 4, text: 'camera', amount: 150}
+//];
+
+
+//for storage
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions')
+);
+let transactions = localStorage.getItem('transactions') != null ? localStorageTransactions : [];
+
+//Add Transaction
+function addTransactions(e) {
+    e.preventDefault();
+
+    
+}
+
+
+
+
+
+
+
+/* const balance = document.querySelector('#balance')
 
 balance.addEventListener('click', function(){
     alert('Hello world')
-})
+}) */
